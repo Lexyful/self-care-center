9b211b869c3b35401437af9bff5637d6a6e23c36
Alexys Jordan <alexyskjordan@gmail.com>
2022-12-15 14:45:33 -0700

    add event listener

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 // var page = document.querySelector()
 var header = document.querySelector.innerText(".h1")
 var header = document.querySelector.innerText(".h2")
+var buttonAddMessage = document.querySelector('button');
+
+buttonAddMessage.addEventListener('click', addMessage);
+
+
+function addMessage() {
+    
+}
