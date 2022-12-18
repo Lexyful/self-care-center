b99ab64636a2c074d501fda22c4e0566a1d795ea
Alexys Jordan <alexyskjordan@gmail.com>
2022-12-18 13:37:43 -0700

    add formatting

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
             "I am full of energy and vitality and my mind is calm and peaceful.",
             "Every day I am getting healthier and stronger.",
             "I honor my body by trusting the signals that it sends me.",
-            "I manifest perfect health by making smart choices."]
+            "I manifest perfect health by making smart choices."];
 
         var mantras = 
              ["Breathing in, I send myself love. Breathing out,      I send love to someone else who needs it.",
@@ -29,39 +29,39 @@
             "This too shall pass.", 
             "Yesterday is not today.",
             "The only constant is change.",
-            "Onward and upward."]
+            "Onward and upward."];
 
 
-        var radioButton1 = document.querySelector(".radio1")
-        var radioButton2 = document.querySelector(".radio2")
-        var buttonAddMessage = document.querySelector(".button")
-        var buttonFavoriteMessage = document.querySelector("#favoriteMessage")
-        var buttonViewFavoriteMessage = document.querySelector("#viewFavoriteMessageBtn")
-        var buttonHome = document.querySelector(".home-button")
-        var sectionMessage = document.querySelector(".uplifting-message")
-        var icon = document.querySelector(".icon")
-        var viewHome = document.querySelector(".home-view")
-        var viewFavoritesSection = document.querySelector(".saved-messages-section")
-        var viewFavoriteList = document.querySelector(".list")
-        var box3 = document.querySelector(".box3")
-        var viewFavoritedMessages = []
+        var radioButton1 = document.querySelector(".radio1");
+        var radioButton2 = document.querySelector(".radio2");
+        var icon = document.querySelector(".icon");
+        var buttonAddMessage = document.querySelector(".button");
+        var buttonFavoriteMessage = document.querySelector("#favoriteMessage");
+        var buttonViewFavoriteMessage = document.querySelector("#viewFavoriteMessageBtn");
+        var buttonHome = document.querySelector(".home-button");
+        var sectionMessage = document.querySelector(".uplifting-message");
+        var viewHome = document.querySelector(".home-view");
+        var viewFavoritesSection = document.querySelector(".saved-messages-section");
+        var viewFavoriteList = document.querySelector(".list");
+        var viewFavoritedMessages = [];
+        var box3 = document.querySelector(".box3");
         
 
 
-            buttonAddMessage.addEventListener('click', addMessage);
-            buttonFavoriteMessage.addEventListener('click', favoriteMessage)
-            buttonViewFavoriteMessage.addEventListener('click', displayFavoriteMessages)
-            buttonHome.addEventListener('click', displayHome)
-            box3.addEventListener('dblclick', deleteMessage)
+            buttonAddMessage.addEventListener("click", addMessage);
+            buttonFavoriteMessage.addEventListener("click", favoriteMessage);
+            buttonViewFavoriteMessage.addEventListener("click", displayFavoriteMessages);
+            buttonHome.addEventListener("click", displayHome);
+            box3.addEventListener("dblclick", deleteMessage);
 
 
         function getRandomIndex(array) {
             return Math.floor(Math.random() * array.length);
-  }
-            console.log(affirmations[getRandomIndex(affirmations)])
+    }
+            console.log(affirmations[getRandomIndex(affirmations)]);
 
         function addMessage() {
-             event.preventDefault();
+            event.preventDefault();
             console.log('here')
             icon.classList.add("hidden")
             sectionMessage.classList.remove("hidden")
@@ -70,10 +70,10 @@
             buttonViewFavoriteMessage.classList.remove("hidden")
             if(radioButton1.checked === true) {
                 sectionMessage.innerText = affirmations[getRandomIndex(affirmations)]
-                console.log('radio1', sectionMessage.innerText)
+                console.log("radio1", sectionMessage.innerText)
             } else if (radioButton2.checked === true) {
                 sectionMessage.innerText = mantras[getRandomIndex(mantras)]
-                console.log('radio2', sectionMessage.innerText)
+                console.log("radio2", sectionMessage.innerText)
                     }
 
 
@@ -82,7 +82,7 @@
     function favoriteMessage() {
         event.preventDefault();
         if (!viewFavoritedMessages.includes(sectionMessage.innerText)) {
-         viewFavoritedMessages.push(sectionMessage.innerText)
+            viewFavoritedMessages.push(sectionMessage.innerText);
        
        }
     } 
@@ -90,46 +90,38 @@
     function displayFavoriteMessages() {
         event.preventDefault();
         viewHome.classList.add("hidden");
-        viewFavoritesSection.classList.remove("hidden") 
-        box3.classList.remove("hidden")
-        buttonHome.classList.remove("hidden")
-        viewFavoriteList.innerHTML = ''
+        viewFavoritesSection.classList.remove("hidden"); 
+        box3.classList.remove("hidden");
+        buttonHome.classList.remove("hidden");
+        viewFavoriteList.innerHTML = ""
         for(var i = 0; i < viewFavoritedMessages.length; i++) {
             viewFavoriteList.innerHTML += `<li>${viewFavoritedMessages[i]}</li>` 
-            console.log('here')
         }
     }
 
     function deleteMessage() {
-        var deletedMessages = event.target
-        console.log(deletedMessages.innerText)
+        var deletedMessages = event.target;
+        console.log(deletedMessages.innerText);
         for (var i = 0; i < viewFavoritedMessages.length; i++) {
         if(deletedMessages.innerText === viewFavoritedMessages[i]) {
-            viewFavoritedMessages.splice(i, 1)
-            displayFavoriteMessages()
-        }
-     } 
+            viewFavoritedMessages.splice(i, 1);
+            displayFavoriteMessages();
+            }
+        } 
     }
  
     
 
     function displayHome() {
-      viewHome.classList.remove("hidden")
-      viewFavoritesSection.classList.add("hidden") 
-      box3.classList.add("hidden")
-      buttonHome.classList.add("hidden") 
-      icon.classList.remove("hidden")
-            sectionMessage.classList.add("hidden")
-            viewFavoritesSection.classList.remove("hidden") 
-            buttonFavoriteMessage.classList.add("hidden")
-            buttonViewFavoriteMessage.classList.add("hidden")
+        viewHome.classList.remove("hidden");
+        viewFavoritesSection.classList.add("hidden"); 
+        box3.classList.add("hidden");
+        buttonHome.classList.add("hidden"); 
+        icon.classList.remove("hidden");
+        sectionMessage.classList.add("hidden");
+        viewFavoritesSection.classList.remove("hidden"); 
+        buttonFavoriteMessage.classList.add("hidden");
+        buttonViewFavoriteMessage.classList.add("hidden");
     }
   
 
-
-// function view favoriteMessagePage() {
-//     array.target.innerText
-
-// }
-
-// e.target?
